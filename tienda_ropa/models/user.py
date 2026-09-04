2b8c13058b69be39934b76e487b766b096431699
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-09-04 16:42:38 -0500

    Create user.py

---
+++ tienda_ropa/models/user.py
@@ -0,0 +1,17 @@
+from database import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    role = db.Column(db.String(20), default='client')
+
+    def set_password(self, plain_password):
+        self.password = generate_password_hash(plain_password)
+
+    def check_password(self, plain_password):
+        return check_password_hash(self.password, plain_password)
